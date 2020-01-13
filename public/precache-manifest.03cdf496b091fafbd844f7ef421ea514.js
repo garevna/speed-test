d1ab43ad19317cc7851ec0f06079bed74eafd9fc
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2020-01-13 12:43:38 +0200

    next step - build app

---
+++ public/precache-manifest.03cdf496b091fafbd844f7ef421ea514.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d368e9398db3eeafb85",
+    "url": "/assets/css/app.1ab48526.css"
+  },
+  {
+    "revision": "3a94fbef08c89c3f92f2",
+    "url": "/assets/css/chunk-vendors.b0e5ac9e.css"
+  },
+  {
+    "revision": "1d368e9398db3eeafb85",
+    "url": "/assets/js/app.a6c04a88.js"
+  },
+  {
+    "revision": "3a94fbef08c89c3f92f2",
+    "url": "/assets/js/chunk-vendors.64b516e1.js"
+  },
+  {
+    "revision": "e5b05553d19c80b834e3f262dd66a9a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08fbd4db6bf4320efb65b69a2380020e",
+    "url": "/manifest.json"
+  }
+]);
